19824b7d9bdeafc7ab1752c6d49ebc2b4995a422
Matt Rota <matt.rota@opensponsorship.com>
2023-12-03 16:17:54 -0500

    styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,22 @@
 	import listings from '$lib/data/lilstings';
 </script>
 
-{#each listings as listing}
-	<h1>{listing.name}</h1>
-	<h2><a href={listing.location}>{listing.location}</a></h2>
-	{#each listing.schedule as offer}
-		<h3>{offer.description}</h3>
+<div class="contianer mx-auto px-10">
+	{#each listings as listing}
+		<div class="">
+			<h1>{listing.name}</h1>
+			<h2><a href={listing.location}>{listing.location}</a></h2>
+			{#each listing.schedule as offer}
+				<h3>{offer.description}</h3>
+			{/each}
+			{#if listing.links?.length}
+				<!-- image holder -->
+				<div class="">
+					{#each listing.links as img}
+						<img alt="img" src={img} />
+					{/each}
+				</div>
+			{/if}
+		</div>
 	{/each}
-	{#if listing.links?.length}
-		{#each listing.links as img}
-			<img alt="img" src={img} />
-		{/each}
-	{/if}
-{/each}
+</div>
