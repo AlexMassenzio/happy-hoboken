be0d8f3dd6d3ef5293719c1cd17c84ce5d856386
Alex Massenzio <alexmassenzio@gmail.com>
2023-12-03 16:23:21 -0500

    Added offer dates to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import listings from '$lib/data/lilstings';
+	import { buildDateFromOffer } from '$lib/offerUtils';
 </script>
 
 <div class="contianer mx-auto px-10">
@@ -8,6 +9,7 @@
 			<h1>{listing.name}</h1>
 			<h2><a href={listing.location}>{listing.location}</a></h2>
 			{#each listing.schedule as offer}
+				<h3>{buildDateFromOffer(offer)}</h3>
 				<h3>{offer.description}</h3>
 			{/each}
 			{#if listing.links?.length}
