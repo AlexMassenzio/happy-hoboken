802fe697dbe57ee3b70fad2b798523b810d06228
Matt Rota <matt.rota@opensponsorship.com>
2023-12-03 15:58:08 -0500

    display listing links as images

--- src/lib/data/lilstings.ts
+++ src/lib/data/lilstings.ts
@@ -1,19 +1,22 @@
 import type { Listing } from '$lib/types/listing';
 
+import stingrayHappyHour from '$lib/assets/images/stingray.png';
+
 const listings: Listing[] = [
-    {
-        name: "Stingray Lounge",
-        location: "https://maps.app.goo.gl/4C7uAeHUenSEcvM2A",
-        schedule: [
-            {
-                startDay: 4,
-                endDay: 4,
-                startHour: 16,
-                endHour: 18,
-                description: "$18 Lobster Rolls & 50% Off Beer"
-            }
-        ]
-    }
+	{
+		name: 'Stingray Lounge',
+		location: 'https://maps.app.goo.gl/4C7uAeHUenSEcvM2A',
+		schedule: [
+			{
+				startDay: 4,
+				endDay: 4,
+				startHour: 16,
+				endHour: 18,
+				description: '$18 Lobster Rolls & 50% Off Beer'
+			}
+		],
+		links: [stingrayHappyHour]
+	}
 ];
 
 export default listings;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,4 +8,9 @@
 	{#each listing.schedule as offer}
 		<h3>{offer.description}</h3>
 	{/each}
+	{#if listing.links?.length}
+		{#each listing.links as img}
+			<img alt="img" src={img} />
+		{/each}
+	{/if}
 {/each}
