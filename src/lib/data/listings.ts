008f907926c3dfa049e10f45c64ccc1c5aef8a81
Matt Rota <matt.rota@opensponsorship.com>
2023-12-03 17:30:17 -0500

    updated listings

--- src/lib/data/listings.ts
+++ src/lib/data/listings.ts
@@ -8,11 +8,39 @@ const listings: Listing[] = [
 		location: 'https://maps.app.goo.gl/4C7uAeHUenSEcvM2A',
 		schedule: [
 			{
+				startDay: 0,
+				endDay: 0,
+				startHour: 13,
+				endHour: 15,
+				description: '50% Off Oysters & Sparkling Wine'
+			},
+			{
+				startDay: 1,
+				endDay: 3,
+				startHour: 16,
+				endHour: 18,
+				description: '50% Off Oysters & Sparkling Wine'
+			},
+			{
 				startDay: 4,
 				endDay: 4,
 				startHour: 16,
 				endHour: 18,
 				description: '$18 Lobster Rolls & 50% Off Beer'
+			},
+			{
+				startDay: 5,
+				endDay: 5,
+				startHour: 16,
+				endHour: 18,
+				description: '$10 House Cocktails'
+			},
+			{
+				startDay: 6,
+				endDay: 6,
+				startHour: 15,
+				endHour: 17,
+				description: '$10 House Cocktails'
 			}
 		],
 		links: [stingrayHappyHour]
