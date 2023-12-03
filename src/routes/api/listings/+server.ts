30cffc980493bf746d89e0a9701790e6b6597ec6
Alex Massenzio <alexmassenzio@gmail.com>
2023-12-03 17:32:35 -0500

    fixed listing file reference

--- src/routes/api/listings/+server.ts
+++ src/routes/api/listings/+server.ts
@@ -1,4 +1,4 @@
-import listings from '$lib/data/lilstings';
+import listings from '$lib/data/listings';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async () => {
