b1a1933cb8e577ff211b20eb36a3ef536ce74ac4
Alex Massenzio <alexmassenzio@gmail.com>
2023-12-03 16:15:57 -0500

    added date formatting for offers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import listings from '$lib/data/lilstings';
+	import { buildDateFromOffer } from '$lib/offerUtils';
 </script>
 
 {#each listings as listing}
 	<h1>{listing.name}</h1>
 	<h2><a href={listing.location}>{listing.location}</a></h2>
 	{#each listing.schedule as offer}
+		<h3>{buildDateFromOffer(offer)}</h3>
 		<h3>{offer.description}</h3>
 	{/each}
 {/each}
